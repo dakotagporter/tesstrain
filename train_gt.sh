a8dd48e5835b2eb268857d55787317f798202dfb
dakotagporter <dakotap3045@gmail.com>
2021-04-11 16:30:41 -0600

    Create automated training script.

--- train_gt.sh
+++ train_gt.sh
@@ -1,8 +1,25 @@
+export TESSDATA_PREFIX=`pwd`/tesseract/tessdata
+
+# Set the TESSDATA_PREFIX (changes made in the Makefile).
+## Ask user what step they are on (Preprocess, Train). Use flags and functions?
+# Run Drew's scripts to convert, process and segment a set of new images.
+# Place the tif and gt.txt files in the data folder.
+# Ask if tif and gt files are edited...abort if not.
+# Begin training.
+
 if [ -d ./data ]
 then
-	echo -e "\n\tFound data directory ..."
+	echo -e "\n\tFound data directory ...\n"
 else
-	echo -e "\n\tCreating data directory ..."
+	echo -e "\n\tCreating data directory ...\n"
 fi
 
-echo -e "\nAll desired image and ground truth duo training files (*.tif, *.gt.txt) MUST be placed in the data directory"
+read -p "All desired image and ground truth duo training files (*.tif, *.gt.txt) MUST be placed in the data directory. Continue? [y/n] " response
+[[ ${response} =~ ^[nN]$ ]] && exit
+
+
+
+
+read -p "Enter the name of the model you wish to continue training from > " start_model
+model_name=`echo ssq_$(date '+%Y%m%d%H%M%S')`
+make training MODEL_NAME=${model_name} START_MODEL=${start_model}
