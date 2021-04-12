eb19ad9b0b02d86668dedcb4e5d3aada72b7fcca
dakotagporter <dakotap3045@gmail.com>
2021-04-12 10:56:16 -0600

    Update training script.

--- train_gt.sh
+++ train_gt.sh
@@ -1,25 +1,39 @@
-export TESSDATA_PREFIX=`pwd`/tesseract/tessdata
-
-# Set the TESSDATA_PREFIX (changes made in the Makefile).
 ## Ask user what step they are on (Preprocess, Train). Use flags and functions?
 # Run Drew's scripts to convert, process and segment a set of new images.
 # Place the tif and gt.txt files in the data folder.
 # Ask if tif and gt files are edited...abort if not.
 # Begin training.
+train () {
+	if [ -d ./data ]
+	then
+		echo -e "\n\tFound data directory ...\n"
+	else
+		echo -e "\n\tCreating data directory ...\n"
+	fi
+	
+	echo -e "All desired image and ground truth duo training files (*.tif, *.gt.txt) MUST be placed in the data directory."
+	read -p "Continue? [y/n] " response
+	[[ ${response} =~ ^[nN]$ ]] && exit
+	
+	
+	echo
+	read -p "Enter the name of your model > " model_name
+	read -p "Enter the name of the model you wish to continue training from > " start_model
+	make training MODEL_NAME=${model_name} START_MODEL=${start_model} PSM=7 TESSDATA=`pwd`/tesseract/tessdata
+}
 
-if [ -d ./data ]
-then
-	echo -e "\n\tFound data directory ...\n"
-else
-	echo -e "\n\tCreating data directory ...\n"
-fi
+preprocess () {
+	echo "Preprocessing"
+}
 
-read -p "All desired image and ground truth duo training files (*.tif, *.gt.txt) MUST be placed in the data directory. Continue? [y/n] " response
-[[ ${response} =~ ^[nN]$ ]] && exit
-
-
-
-
-read -p "Enter the name of the model you wish to continue training from > " start_model
-model_name=`echo ssq_$(date '+%Y%m%d%H%M%S')`
-make training MODEL_NAME=${model_name} START_MODEL=${start_model}
+while getopts pt flag
+do
+	case "${flag}" in
+		p) preprocess
+			;;
+		t) train
+			;;
+		*) echo -e "Invalid option: -$flag"
+			;;
+	esac
+done
